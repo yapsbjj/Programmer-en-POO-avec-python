90224d8ff915156c928c99073fe988b8c861930a
unknown <ya.ramdane01@gmail.com>
2024-06-14 15:10:35 +0200

    add final files

--- mediatheque/media/media/urls.py
+++ mediatheque/media/media/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('media_project.urls')),
     path('users_app/', include('users_app.urls')),
 ]
